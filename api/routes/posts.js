c49d647df1a437b98f89731da493b3c4f6b7f395
Adriel Modollo <adrielmodollo@outlook.com>
2021-10-31 01:53:37 -0300

    Retornar todos os posts com condição de usuário e categoria

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -69,4 +69,28 @@ router.get("/:id", async (req, res) => {
     }
   });
 
+  //Retornar todos os posts com condição de usuário e categoria
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+      let posts;
+      if (username) {
+        posts = await Post.find({ username });
+      } else if (catName) {
+        posts = await Post.find({
+          categories: {
+            $in: [catName],
+          },
+        });
+      } else {
+        posts = await Post.find();
+      }
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 module.exports = router;
