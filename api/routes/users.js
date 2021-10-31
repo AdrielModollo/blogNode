42e3b701db3b9edf71e9c976020dc496eb4238af
Adriel Modollo <adrielmodollo@outlook.com>
2021-10-31 00:12:26 -0300

    Delete

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require('bcrypt');
+
 
 //Atualiza
 router.put("/:id", async(req,res) => {
@@ -12,7 +14,9 @@ router.put("/:id", async(req,res) => {
         try{
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set:req.body,
-            });
+            },
+            {new:true}//envie este novo usuário atualizado
+        );
             res.status(200).json(updatedUser);
         } catch(err){
             res.status(500).json(err);
@@ -23,5 +27,26 @@ router.put("/:id", async(req,res) => {
 });
 
 //Deleta
+router.delete("/:id", async(req,res) => {
+    if(req.body.userId === req.params.id) {
+        try{
+            const user = await User.findById(req.params.id);
+            try{
+                await Post.deleteMany({username: user.username});
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("O usuário foi deletado...");
+            } catch(err){
+                res.status(500).json(err);
+            } 
+        } catch(err) {
+            res.status(404).json("Usuário não encontrado!");
+        }
+    } else {
+        res.status(401).json("Você pode deletar apenas sua conta!");
+    }
+});
+
+
+  
 
 module.exports = router;
